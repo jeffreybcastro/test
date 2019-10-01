b9b1658e6aeec29d2dc83412d5b525e2587eaf5a
Jeffrey Castro <jeffreybcastro@gmail.com>
2019-09-30 20:41:29 -0600

    Updated init.py and manifest file

--- l10n_hn_check_printing/__manifest__.py
+++ l10n_hn_check_printing/__manifest__.py
@@ -1,35 +1,24 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "my_module",
-
-    'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
-
+    'name': 'HN Checks Layout',
+    'version': '1.0',
+    'category': 'Accounting',
+    'summary': 'Print HN Checks',
     'description': """
-        Long description of module's purpose
+This module allows to print your payments on pre-printed check paper.
+You can configure the output (layout, stubs informations, etc.) in company settings, and manage the
+checks numbering (if you use pre-printed checks without numbers) in journal settings.
     """,
-
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
-
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
-    # for the full list
-    'category': 'Uncategorized',
-    'version': '0.1',
-
-    # any module necessary for this one to work correctly
-    'depends': ['base'],
-
-    # always loaded
+    'website': 'http://www.d2i-solutions.com',
+    'depends' : ['account_check_printing', 'l10n_hn'],
     'data': [
-        # 'security/ir.model.access.csv',
-        'views/views.xml',
-        'views/templates.xml',
+        'data/hn_check_printing.xml',
+        'report/print_check.xml',
+        'report/print_check_top.xml',
+        'report/print_check_middle.xml',
+        'report/print_check_bottom.xml',
     ],
-    # only loaded in demonstration mode
-    'demo': [
-        'demo/demo.xml',
-    ],
+    'installable': True,
+    'auto_install': True,
+    'license': 'OEEL-1',
 }
