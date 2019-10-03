7c1da93386eb4be76f7ce0fd864a72ec4b93a6d6
Jeffrey Castro <jeffreybcastro@gmail.com>
2019-10-03 09:44:33 -0600

    Update account_payment.py

--- l10n_hn_check_printing/models/account_payment.py
+++ l10n_hn_check_printing/models/account_payment.py
@@ -9,8 +9,7 @@ class account_payment(models.Model):
     def do_print_checks(self):
         if self:
             check_layout = self[0].company_id.account_check_printing_layout
-            # A config parameter is used to give the ability to use this check format even in other countries than US, as not all the localizations have one
-            if check_layout != 'disabled' and (self[0].journal_id.company_id.country_id.code == 'HN' or bool(self.env['ir.config_parameter'].sudo().get_param('account_check_printing_force_us_format'))):
+            if check_layout != 'disabled' and (self[0].journal_id.company_id.country_id.code == 'HN'):
                 self.write({'state': 'sent'})
                 return self.env.ref('l10n_hn_check_printing.%s' % check_layout).report_action(self)
         return super(account_payment, self).do_print_checks()
